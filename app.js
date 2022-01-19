9345acb7a82035b5096299b08522a1f198480532
Juanfelidc <juanfelipediezc@gmail.com>
2022-01-19 06:40:56 -0500

    Actualizando app.js listen

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const path =require('path');
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000,() => {
     console.log('Servidor corriendo en el puerto 3000')
 });
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.sendFile(__dirname +'/views/home.html');
 })
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
